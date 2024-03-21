4c84a3946eddedbaedbc482822b1c73ae589c7bf
Aravind-mca2023 <139735245+Aravind-mca2023@users.noreply.github.com>
2024-03-21 12:09:41 +0530

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -2,11 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button';
 import userContext from "./context.js";
 import bank from './bank.jpg';
-import Mycarousel from './carousel.js';
+
 
 export default function Homepage(){
     return(<>
-    {/* <Mycarousel></Mycarousel> */}
+
     <img id='bank' src={bank} height='500' width='900'></img>
     </>)
 }
